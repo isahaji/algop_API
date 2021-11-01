a50e5bdd44757327bdf7db9fe1e15058cacc071e
isahaji <isa@isahaji.com>
2021-11-01 09:26:42 +0300

    init

---
+++ README.md
@@ -0,0 +1,19 @@
+### What is Algop API?
+
+Algop API is a collection of common used algorithms implemented in python as functions which can be fetched
+
+## Base URL
+
+    https://xxx.xxx/
+
+| query | Default | Purpose | E.g              |
+| ----- | ------- | ------- | ---------------- |
+| array | none    | N/A     | /?q=6&q=1&q=4    |
+<!--  | say     | false   | Will print array | --> 
+
+## Current
+
+| Algorithm      | Done | endpoint   |
+| -------------- | ---- | ---------- |
+| Bubble sort    | ✅   | /bubble    |
+| Insertion sort | ✅   | /insertion |
---
+++ main.py
@@ -0,0 +1,24 @@
+from typing import List, Optional
+from algop import bubbleSort, insertionSort
+from fastapi import FastAPI, Query
+app = FastAPI(
+    title="Algop",
+    description="Common Algorithms",
+    version="1.0",
+    contact={"url":"https://isahaji.com","name": "Isa Haji", "email": "isa@isahaji.com"}
+)
+
+
+@app.get('/')
+async def main():
+    return "Welcome to Algop API"
+
+
+@app.get("/bubble/")
+async def bubblesort(q: Optional[List[str]] = Query(None)):
+    return {"array": bubbleSort(q)}
+
+
+@app.get('/insertion')
+async def insertionsort(q: Optional[List[str]] = Query(None)):
+    return {"array": insertionSort(q)}
